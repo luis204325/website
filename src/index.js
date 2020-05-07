199a56aa5a5769090a6b0c7bd40df3ae1693f734
Luis Alexander Contreras Alfaro <luis.contreras@grupocalvo.com.sv>
2020-05-07 00:48:50 -0600

    primera version

---
+++ src/index.js
@@ -0,0 +1,10 @@
+ //creamos las contantes con los modulos que vamos a necesitar 
+ const express = require("express");
+ const app = express(); //arrancamos express
+ const path = require ('path'); //path nos ayuda a saber que hay dentro de cada carpeta 
+
+ app.use(express.static(path.join(__dirname, 'public'))); //obtenemos la ruta de public para servir el html al js. 
+
+ app.listen(3000,() => {
+     console.log('server on port 3000');
+ });
